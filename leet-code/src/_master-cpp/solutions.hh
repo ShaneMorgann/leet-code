e830d7a8bd8f04a588821854b5c7e7e2e5064d72
Shane M <ShaneMorgann@gmail.com>
2024-11-16 06:53:36 -0500

    c++ solution namespace updated to use single line

--- leet-code/src/_master-cpp/solutions.hh
+++ leet-code/src/_master-cpp/solutions.hh
@@ -1,3 +1,10 @@
-namespace _0001{
-    void run();
-}
+namespace _0001{void run();}
+//namespace _0002{void run();}
+//namespace _0003{void run();}
+//namespace _0004{void run();}
+//namespace _0005{void run();}
+//namespace _0006{void run();}
+//namespace _0007{void run();}
+//namespace _0008{void run();}
+//namespace _0009{void run();}
+//namespace _0010{void run();}
